d40020092119031c8d848daf6044b0294f2972da
sebastianselander <sebastian.selander@gmail.com>
2024-11-01 15:03:01 +0100

    add aoc23 day 2 example

---
+++ aoclude/src/Example/Day_23_02.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use camelCase" #-}
+
+module Example.Day_23_02 where
+
+import Format
+
+data Color = Color_red | Color_green | Color_blue
+    deriving Show
+
+intro
+
+parse :: String -> [(Int, [[(Int, Color)]])]
+parse = [fmt|(Game %d: ((%d @Color)&(, )&(; ))%n)*|]
+
+input :: String
+input = 
+    "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\n\
+    \Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\n\
+    \Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\n\
+    \Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\n\
+    \Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green\n"
