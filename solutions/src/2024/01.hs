b70ca4be7805ab08cfdaeb0364d79a402a662214
sebastianselander <sebastian.selander@gmail.com>
2024-12-01 06:09:11 +0100

    day 1

--- solutions/src/2024/01.hs
+++ solutions/src/2024/01.hs
@@ -1,4 +1,14 @@
 module Main where
+import Advent.Format (format,fmt)
+import Data.List
+import Advent.Prelude (countElem)
 
 main :: IO ()
-main = putStrLn "I can't wait!"
+main = do
+    inp <- readFile "inputs/2024/01.txt"
+    let (lefts, rights) = unzip $ [fmt|((%i   %i)%n)*|] inp
+    let xs = sort lefts
+    let ys = sort rights
+    print $ sum $ zipWith (\x y -> abs $ x - y ) xs ys
+    print $ sum [ x * countElem x ys | x <- xs ]
+
