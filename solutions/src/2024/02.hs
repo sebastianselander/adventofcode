af1548e0442edbde232c0eb33b35923a1e3204a1
sebastianselander <sebastian.selander@gmail.com>
2024-12-02 06:56:44 +0100

    clean up day 2

--- solutions/src/2024/02.hs
+++ solutions/src/2024/02.hs
@@ -1,44 +1,20 @@
 module Main where
 
-import Advent.Box
-import Advent.Chinese
-import Advent.Coord
-import Advent.Coord3
-import Advent.Flow
-import Advent.Format
-import Advent.Group
-import Advent.Nat
-import Advent.PQueue
-import Advent.Permutation
-import Advent.Prelude
-import Advent.Queue
-import Advent.ReadS
-import Data.List (delete, tails)
-import GHC.RTS.Flags (ProfFlags (descrSelector))
+import Advent.Format (format)
+import Advent.Prelude (countOn, deleteAt)
+import Data.List
 
 main :: IO ()
 main = do
-    file <- readFile "inputs/2024/test.txt"
-    real <- readFile "inputs/2024/02.txt"
-    let input = [fmt|(%i& %n)*|] real
-    let xs = fmap (\x -> (decreasing x || increasing x) && diff x) input
-    print (countElem False xs)
-    print $ countElem True $ fmap (any ok) $ fmap foo input
+    input <- [format|2024 2 (%i& %n)*|]
+    print $ countOn id $ fmap safe input
+    print $ countOn id $ fmap (any safe . removals) input
 
-increasing :: [Int] -> Bool
-increasing (x : y : xs) = x < y && increasing (y : xs)
-increasing _ = True
+safe :: [Int] -> Bool
+safe x = (byPair (<) x || byPair (>) x) && byPair (\x y -> abs (x - y) >= 1 && abs (x - y) <= 3) x
+  where
+    byPair f (x : y : xs) = f x y && byPair f (y : xs)
+    byPair _ _ = True
 
-decreasing :: [Int] -> Bool
-decreasing (x : y : xs) = x > y && decreasing (y : xs)
-decreasing _ = True
-
-diff :: [Int] -> Bool
-diff (x : y : xs) = abs (x - y) >= 1 && abs (x - y) <= 3 && diff (y : xs)
-diff _ = True
-
-ok :: [Int] -> Bool
-ok xs = (\x -> (increasing x || decreasing x) && diff x) $ xs
-
-foo :: [Int] -> [[Int]]
-foo xs = [h <> t | n <- [0 .. length xs - 1], let (h, _ : t) = splitAt n xs]
+removals :: [Int] -> [[Int]]
+removals xs = [deleteAt n xs | n <- [0 .. length xs - 1]]
