4e21836f0f929f7f05847506e538a30b29644493
sebastianselander <sebastian.selander@gmail.com>
2024-12-03 06:52:53 +0100

    fix both parts

--- solutions/src/2024/03.hs
+++ solutions/src/2024/03.hs
@@ -1,38 +1,41 @@
 module Main where
 
 import Advent.Format
+import Data.Char (digitToInt, isDigit)
 import Data.List (isPrefixOf)
-import Text.RE.TDFA
-import Data.Char (isDigit, digitToInt)
 import Debug.Trace (traceShow)
+import Text.RE.TDFA
 
 main :: IO ()
 main = do
     input <- readFile "/home/sebastian/Documents/git/adventofcode/inputs/2024/03.txt"
-    print $ sum $ fmap eval $ instr True input
+    print $ sum (eval <$> instr True True input)
+    print $ sum (eval <$> instr True False input)
 
 data Instr = Mul Int Int | BadMul Int Int
-    deriving Show
-toInt s 
-  | all isDigit s = decimal $ fmap digitToInt s
-  | otherwise = 0 
+    deriving (Show)
+toInt s
+    | all isDigit s = decimal $ fmap digitToInt s
+    | otherwise = 0
 
+eval :: Instr -> Int
 eval (Mul x y) = x * y
 eval _ = 0
- 
-mul b = if b then Mul else BadMul 
 
-instr :: Bool -> String -> [Instr]
-instr lcont [] = []
-instr lcont ('d':'o':'(':')':rest) = instr True rest
-instr lcont ('d':'o':'n':'\'':'t':'(':')':rest) = instr False rest
-instr lcont ('m':'u':'l':'(':x:',':a:')':rest) = mul lcont (toInt [x]) (toInt [a]) : instr lcont rest
-instr lcont ('m':'u':'l':'(':x:',':a:b:')':rest) = mul lcont (toInt [x]) (toInt [a,b]) : instr lcont rest
-instr lcont ('m':'u':'l':'(':x:',':a:b:c:')':rest) = mul lcont (toInt [x]) (toInt [a,b,c]) : instr lcont rest
-instr lcont ('m':'u':'l':'(':x:y:',':a:')':rest) = mul lcont (toInt [x,y]) (toInt [a]) : instr lcont rest
-instr lcont ('m':'u':'l':'(':x:y:',':a:b:')':rest) = mul lcont (toInt [x,y]) (toInt [a,b]) : instr lcont rest
-instr lcont ('m':'u':'l':'(':x:y:',':a:b:c:')':rest) = mul lcont (toInt [x,y]) (toInt [a,b,c]) : instr lcont rest
-instr lcont ('m':'u':'l':'(':x:y:z:',':a:')':rest) = mul lcont (toInt [x,y,z]) (toInt [a]) : instr lcont rest
-instr lcont ('m':'u':'l':'(':x:y:z:',':a:b:')':rest) = mul lcont (toInt [x,y,z]) (toInt [a,b]) : instr lcont rest
-instr lcont ('m':'u':'l':'(':x:y:z:',':a:b:c:')':rest) = mul lcont (toInt [x,y,z]) (toInt [a,b,c]) : instr lcont rest
-instr lcont (x:xs) = instr lcont xs
+mul :: Bool -> Int -> Int -> Instr
+mul b = if b then Mul else BadMul
+
+instr :: Bool -> Bool -> String -> [Instr]
+instr lcont to [] = []
+instr lcont to ('d' : 'o' : '(' : ')' : rest) = instr True to rest
+instr lcont to ('d' : 'o' : 'n' : '\'' : 't' : '(' : ')' : rest) = instr to to rest
+instr lcont to ('m' : 'u' : 'l' : '(' : x : ',' : a : ')' : rest) = mul lcont (toInt [x]) (toInt [a]) : instr lcont to rest
+instr lcont to ('m' : 'u' : 'l' : '(' : x : ',' : a : b : ')' : rest) = mul lcont (toInt [x]) (toInt [a, b]) : instr lcont to rest
+instr lcont to ('m' : 'u' : 'l' : '(' : x : ',' : a : b : c : ')' : rest) = mul lcont (toInt [x]) (toInt [a, b, c]) : instr lcont to rest
+instr lcont to ('m' : 'u' : 'l' : '(' : x : y : ',' : a : ')' : rest) = mul lcont (toInt [x, y]) (toInt [a]) : instr lcont to rest
+instr lcont to ('m' : 'u' : 'l' : '(' : x : y : ',' : a : b : ')' : rest) = mul lcont (toInt [x, y]) (toInt [a, b]) : instr lcont to rest
+instr lcont to ('m' : 'u' : 'l' : '(' : x : y : ',' : a : b : c : ')' : rest) = mul lcont (toInt [x, y]) (toInt [a, b, c]) : instr lcont to rest
+instr lcont to ('m' : 'u' : 'l' : '(' : x : y : z : ',' : a : ')' : rest) = mul lcont (toInt [x, y, z]) (toInt [a]) : instr lcont to rest
+instr lcont to ('m' : 'u' : 'l' : '(' : x : y : z : ',' : a : b : ')' : rest) = mul lcont (toInt [x, y, z]) (toInt [a, b]) : instr lcont to rest
+instr lcont to ('m' : 'u' : 'l' : '(' : x : y : z : ',' : a : b : c : ')' : rest) = mul lcont (toInt [x, y, z]) (toInt [a, b, c]) : instr lcont to rest
+instr lcont to (x : xs) = instr lcont to xs
