5c72410ed71f8d98e5ee358f3ff950b4426f70dd
sebastianselander <sebastian.selander@gmail.com>
2024-12-04 09:39:32 +0100

    adapt 03

--- solutions/src/2024/03.hs
+++ solutions/src/2024/03.hs
@@ -1,17 +1,24 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Main where
 
-import Advent.Format (format)
+import Advent.Format (format, intro)
 import Data.Maybe (catMaybes)
+
+data Enabled = Enabled_don't_LPAREN_RPAREN | Enabled_do_LPAREN_RPAREN 
+    deriving (Show)
+
+intro
 
 main :: IO ()
 main = do
-    input <- catMaybes <$> [format|2024 3 (mul\(%i,%i\)|do\(\)|don't\(\)|~%t)*|]
+    input <- [format|2024 3 (mul\(%i,%i\)|@Enabled|~%t)*|]
     print $ eval True True input
     print $ eval True False input
 
-eval :: Bool -> Bool -> [Maybe (Maybe (Int, Int))] -> Int
-eval True to ((Just (Just (n, m))) : xs) = n * m + eval True to xs
-eval b to ((Just Nothing) : xs) = eval True to xs
-eval b to (Nothing : xs) = eval to to xs
-eval b to (x:xs) = eval b to xs
+eval :: Bool -> Bool -> [Maybe (Either (Int, Int) Enabled)] -> Int
+eval True to ((Just (Left (n, m))) : xs) = n * m + eval True to xs
+eval b to ((Just (Right Enabled_do_LPAREN_RPAREN)) : xs) = eval True to xs
+eval b to (Just (Right Enabled_don't_LPAREN_RPAREN) : xs) = eval to to xs
+eval b to (_ : xs) = eval b to xs
 eval b to [] = 0
