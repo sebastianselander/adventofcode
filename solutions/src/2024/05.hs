dad9bdde879006c05803ea2aeec539026880ac74
sebastianselander <sebastian.selander@gmail.com>
2024-12-05 07:06:26 +0100

    clean up day 5

--- solutions/src/2024/05.hs
+++ solutions/src/2024/05.hs
@@ -1,51 +1,41 @@
 module Main where
 
-import Advent.Format
-import Advent.Prelude
-import Data.List
-import Data.Set (Set)
-import Data.Set qualified as Set
-import Data.Either
-import Data.Maybe (mapMaybe, fromJust)
+import Advent.Format (format)
+import Control.Arrow (first)
+import Data.Either (partitionEithers)
+import Data.List (partition, sortBy)
+import Data.Maybe (catMaybes, fromJust, mapMaybe)
+import Data.Tuple (swap)
 
--- manually removed empty line in input 
 main :: IO ()
 main = do
-    input <- [format|2024 5 (((%i\|%i)|%i&,)%n)*|]
-    let (rules', list) = partitionEithers input
-    let rules = fmap (\(a,b) -> (b,a)) rules'
-    -- print $ sum (middle <$> filter (inOrder mempty rules) list)
-    let xs = filter (not . inOrder mempty rules) list
-    -- print $ sum $ fmap (middle . fixOrder rules') xs
-    print $ sum $ fmap (middle . fmap fst . fix . fixOrder' rules) xs
+    input <- catMaybes <$> [format|2024 5 (%n|((%i\|%i)|%i&,)%n)*|]
+    let (rules, list) = first (fmap swap) $ partitionEithers input
+    let (goods, bads) = partition (inOrder mempty rules) list
+    print $ sum $ fmap middle goods
+    print $ sum $ fmap (middle . fixOrder rules) bads
 
-get :: Int -> [(Int,Int)] -> [Int]
-get n = mapMaybe f
+lookupRule :: Int -> [(Int, Int)] -> [Int]
+lookupRule n = mapMaybe f
   where
-    f (a,b) | a == n = Just b
-            | otherwise = Nothing
+    f (a, b)
+        | a == n = Just b
+        | otherwise = Nothing
 
-inOrder :: Set Int -> [(Int,Int)] -> [Int] -> Bool
+inOrder :: [Int] -> [(Int, Int)] -> [Int] -> Bool
 inOrder !seen rules [] = True
-inOrder !seen rules (x:xs)
-  | x `Set.member` seen = False
-  | otherwise = case get x rules of
-      ys -> inOrder (Set.fromList ys <> seen) rules xs
-
+inOrder !seen rules (x : xs)
+    | x `elem` seen = False
+    | otherwise = case lookupRule x rules of
+        ys -> inOrder (ys <> seen) rules xs
 
 middle :: [Int] -> Int
-middle xs = let (as,bs) = splitAt (length xs `div` 2) xs in head bs
+middle xs = xs !! (length xs `div` 2)
 
-fixOrder :: [(Int,Int)] -> [Int] -> [Int]
-fixOrder rules xs = fromJust $ find (inOrder mempty rules) $ permutations xs
-
-fixOrder' :: [(Int,Int)] -> [Int] -> [(Int,[Int])]
-fixOrder' rules xs = [ (x,ys) | x <- xs, let ys = get x rules ]
-
-fix :: [(Int,[Int])] -> [(Int,[Int])]
-fix = sortBy f 
+fixOrder :: [(Int, Int)] -> [Int] -> [Int]
+fixOrder rules xs = fst <$> sortBy f [(x, ys) | x <- xs, let ys = lookupRule x rules]
   where
-    f (a,xs) (b,ys) 
-      | a `elem` ys = GT
-      | b `elem` xs = LT
-      | otherwise = EQ
+    f (a, xs) (b, ys)
+        | a `elem` ys = GT
+        | b `elem` xs = LT
+        | otherwise = EQ
