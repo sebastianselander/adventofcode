d82e6faceec599369ea139a83651c7258b81c3cf
sebastianselander <sebastian.selander@gmail.com>
2024-12-06 18:08:21 +0100

    day 6: unboxed array

--- solutions/src/2024/06.hs
+++ solutions/src/2024/06.hs
@@ -1,13 +1,9 @@
 module Main where
 
-import Advent.Coord (
-    Coord,
-    north,
-    turnRight,
- )
+import Advent.Coord ( Coord, north, turnRight, boundingBox )
 import Advent.Format (getArrayInput)
 import Advent.Prelude (arrIx, countBy)
-import Data.Array (Array, assocs, (//))
+import Data.Array.Unboxed (assocs, (//), UArray)
 import Data.List.Extra (nubOrdOn)
 import Data.Set (Set, insert, member, toList)
 
@@ -16,19 +12,13 @@ main = do
     grid <- getArrayInput 2024 6
     let start = head [k | (k, '^') <- assocs grid]
     let walked = nubOrdOn snd $ toList (walk mempty north start grid)
-    print $ length walked
-    print $ countBy null [walk mempty north start (grid // [(k, '#')]) | (_, k) <- walked]
+    print (length walked)
+    print (countBy null [walk mempty north start (grid // [(k, '#')]) | (_, k) <- walked, k /= start])
 
-walk ::
-    Set (Coord, Coord) ->
-    Coord ->
-    Coord ->
-    Array Coord Char ->
-    Set (Coord, Coord)
-walk visited d p grid =
-    if (d, p) `member` visited
-        then mempty
-        else case arrIx grid (d + p) of
-            Nothing -> insert (d, p) visited
-            Just '#' -> walk visited (turnRight d) p grid
-            Just _ -> walk (insert (d, p) visited) d (d + p) grid
+walk :: Set (Coord, Coord) -> Coord -> Coord -> UArray Coord Char -> Set (Coord, Coord)
+walk visited d p grid
+    | (d, p) `member` visited = mempty
+    | otherwise = case arrIx grid (d + p) of
+        Nothing -> insert (d, p) visited
+        Just '#' -> walk visited (turnRight d) p grid
+        Just _ -> walk (insert (d, p) visited) d (d + p) grid
