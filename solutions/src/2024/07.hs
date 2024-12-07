967891f2c1457ec81ce8c5dfd756ef92179b5b7d
sebastianselander <sebastian.selander@gmail.com>
2024-12-07 06:57:07 +0100

    clean up

--- solutions/src/2024/07.hs
+++ solutions/src/2024/07.hs
@@ -1,34 +1,24 @@
 module Main where
 
-import Advent.Format (format)
+import Advent.Format ( format )
 import Data.List.Extra (find)
-import Data.Maybe (mapMaybe)
+import Data.Maybe ( mapMaybe )
 
 main :: IO ()
-main = do
+main = do 
     input <- [format|2024 7 (%i: (%i& )%n)*|]
-    print $ sum $ mapMaybe calibrate input
-    print $ sum $ mapMaybe calibrate2 input
+    print $ sum $ mapMaybe (calibrate [(*), (+)]) input 
+    print $ sum $ mapMaybe (calibrate [(*), (+), cat]) input 
 
-calibrate :: (Int, [Int]) -> Maybe Int
-calibrate (_, []) = Nothing
-calibrate (result, y : ys) = find (== result) $ go y ys
+calibrate :: [Int -> Int -> Int] -> (Int, [Int]) -> Maybe Int
+calibrate _ (_, []) = Nothing
+calibrate fs (result,y:ys) = find (==result) $ go y ys
   where
     go :: Int -> [Int] -> [Int]
     go acc [] = [acc]
-    go acc (x : xs)
+    go acc (x:xs) 
         | acc > result = []
-        | otherwise = go (acc + x) xs <> go (acc * x) xs
-
-calibrate2 :: (Int, [Int]) -> Maybe Int
-calibrate2 (_, []) = Nothing
-calibrate2 (result, y : ys) = find (== result) $ go y ys
-  where
-    go :: Int -> [Int] -> [Int]
-    go acc [] = [acc]
-    go acc (x : xs)
-        | acc > result = []
-        | otherwise = go (acc `cat` x) xs <> go (x + acc) xs <> go (x * acc) xs
+        | otherwise = concat [ go (f acc x) xs | f <- fs]
 
 cat :: Int -> Int -> Int
 cat n m = read $ show n <> show m
