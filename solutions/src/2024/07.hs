530a9dcd8dcb0aee52f6de6a6c6a3da25c12dd45
sebastianselander <sebastian.selander@gmail.com>
2024-12-07 10:29:23 +0100

    beautifuller

--- solutions/src/2024/07.hs
+++ solutions/src/2024/07.hs
@@ -4,12 +4,13 @@ import Advent.Format (format)
 import Advent.Prelude (fixed)
 import Data.List.Extra (find)
 import Data.Maybe (mapMaybe)
+import Prelude hiding ((||))
 
 main :: IO ()
 main = do
     input <- [format|2024 7 (%i: (%i& )%n)*|]
     print $ sum $ mapMaybe (calibrate [(*), (+)]) input
-    print $ sum $ mapMaybe (calibrate [(*), (+), cat]) input
+    print $ sum $ mapMaybe (calibrate [(*), (+), (||)]) input
 
 calibrate :: [Int -> Int -> Int] -> (Int, [Int]) -> Maybe Int
 calibrate _ (_, []) = Nothing
@@ -21,5 +22,5 @@ calibrate fs (result, y : ys) = find (== result) $ go y ys
         | acc > result = []
         | otherwise = concat [go (f acc x) xs | f <- fs]
 
-cat :: Int -> Int -> Int
-cat n m = n * fixed (> m) (* 11) 10 + m
+(||) :: Int -> Int -> Int
+(||) n m = n * fixed (> m) (* 10) 10 + m
