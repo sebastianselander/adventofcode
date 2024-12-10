0318e45e4fbd27b1c8fd8ff65d180ba1a6d932f0
sebastianselander <sebastian.selander@gmail.com>
2024-12-10 12:43:30 +0100

    unboxed array

--- solutions/src/2024/10.hs
+++ solutions/src/2024/10.hs
@@ -3,20 +3,20 @@ module Main where
 import Advent.Coord (Coord, cardinalOn, coordArray)
 import Advent.Format (format)
 import Advent.Prelude (arrIx)
-import Data.Array (Array, assocs, (!))
-import Data.List.Extra (nubOrd)
+import Data.Array.Base (UArray, assocs, (!))
 import Data.Bool (bool)
+import Data.List.Extra (nubOrd)
 
 main :: IO ()
 main = do
-    grid <- coordArray @Array <$> [format|2024 10 (%d*%n)*|]
-    let starts = [ix | (ix, n) <- assocs grid, n == 0] 
+    grid <- coordArray @UArray <$> [format|2024 10 (%d*%n)*|]
+    let starts = [ix | (ix, n) <- assocs grid, n == 0]
     let walked = [walk grid s | s <- starts]
     print $ length $ concatMap nubOrd walked
     print $ length $ concat walked
 
-walk :: Array Coord Int -> Coord -> [Coord]
-walk grid s = bool id (s:) (current == 9) $ concatMap (walk grid) (next s)
+walk :: UArray Coord Int -> Coord -> [Coord]
+walk grid s = bool id (s :) (current == 9) $ concatMap (walk grid) (next s)
   where
     current = grid ! s
     next = cardinalOn ((Just (current + 1) ==) . arrIx grid)
