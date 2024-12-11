b9e63b712893a9f48edbb053fc7b2eb8629720b3
sebastianselander <sebastian.selander@gmail.com>
2024-12-11 09:13:07 +0100

    small change

--- solutions/src/2024/11.hs
+++ solutions/src/2024/11.hs
@@ -4,10 +4,11 @@ import Advent.Format (format)
 import Data.IntMap (IntMap)
 import Data.IntMap qualified as Map
 import Data.IntMap.Strict ((!))
+import Data.Tuple.Extra (both)
 
 main :: IO ()
 main = do
-    input <- [format|2024 11 (%i& )%n|]
+    input <- [format|2024 11 %i& %n|]
     let blinked = iterate run $ Map.fromList [(k, 1) | k <- input]
     let stones = sum . Map.elems
     print $ stones $ blinked !! 25
@@ -21,15 +22,13 @@ run m = Map.fromListWith (+) $ concatMap blink (Map.keys m)
         let keyCount = m ! key
          in case rule key of
                 Left n -> [(n, keyCount)]
-                Right [x, y] -> [(x, keyCount), (y, keyCount)]
-                _ -> error "cant happen"
+                Right (x, y) -> [(x, keyCount), (y, keyCount)]
 
-rule :: Int -> Either Int [Int]
+rule :: Int -> Either Int (Int, Int)
 rule 0 = Left 1
 rule n
-    | even len = Right [read l, read r]
-    | otherwise = Left $ 2024 * n
+    | even len = Right $ both read $ splitAt (len `div` 2) n'
+    | otherwise = Left (2024 * n)
   where
-    (l, r) = splitAt (len `div` 2) n'
     n' = show n
     len = length n'
