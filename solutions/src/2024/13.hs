a5f1ceb7b528f0efbb4f73eefc366787ba19f75c
sebastianselander <sebastian.selander@gmail.com>
2024-12-13 11:56:33 +0100

    day 13

--- solutions/src/2024/13.hs
+++ solutions/src/2024/13.hs
@@ -7,28 +7,13 @@ import Data.Maybe (mapMaybe)
 main :: IO ()
 main = do
     input <- [format|2024 13 (Button A: X+%i, Y+%i%nButton B: X+%i, Y+%i%nPrize: X=%i, Y=%i%n%n)*|]
-    let doubles =
-            fmap
-                ( \(a, b, c, d, e, f) ->
-                    ( fromIntegral a
-                    , fromIntegral b
-                    , fromIntegral c
-                    , fromIntegral d
-                    , fromIntegral e
-                    , fromIntegral f
-                    )
-                )
-                input
-    print $ sum $ mapMaybe (solve 0) doubles
-    print $ sum $ mapMaybe (solve 10000000000000) doubles
+    print $ sum $ mapMaybe (solve 0) input
+    print $ sum $ mapMaybe (solve 10_000_000_000_000) input
 
--- Boring
-solve :: Double -> (Double, Double, Double, Double, Double, Double) -> Maybe Integer
+solve :: Int -> (Int, Int, Int, Int, Int, Int) -> Maybe Int
 solve extra (xa, ya, xb, yb, xp, yp) = do
-    let n = (yb * (xp + extra) - xb * (yp + extra)) / (yb * xa - xb * ya)
-    let m = ((xp + extra) - n * xa) / xb
-    guard $ isInt n && isInt m
-    pure (3 * floor n + floor m)
-
-isInt :: Double -> Bool
-isInt x = abs (fromIntegral @Integer (floor x) - x) < 0.01
+    let n = (yb * (xp + extra) - xb * (yp + extra)) `div` (yb * xa - xb * ya)
+    let m = (xp + extra - n * xa) `div` xb
+    guard $ (n * xa + m * xb) == (xp + extra)
+    guard $ (n * ya + m * yb) == (yp + extra)
+    pure (3 * n + m)
