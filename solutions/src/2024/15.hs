1a7a8c93da591053436f4a0d93270c70751a7003
sebastianselander <sebastian.selander@gmail.com>
2024-12-15 10:34:19 +0100

    clean up day 15

--- solutions/src/2024/15.hs
+++ solutions/src/2024/15.hs
@@ -1,72 +1,71 @@
 module Main where
 
-import Advent.Coord
-import Advent.Format (getRawInput)
-import Data.List.Extra (splitOn, trimEnd)
+import Advent.Coord (
+    Coord (..),
+    charToCoord,
+    coordLines,
+    left,
+    right,
+ )
+import Advent.Format (format)
+import Data.List (foldl')
 import Data.Map (Map, (!))
 import Data.Map qualified as Map
 
 main :: IO ()
 main = do
-    (grid, Just moves) <- parse <$> getRawInput 2024 15
-    print $ sum [100 * r + c | (C r c, 'O') <- Map.toList (foldl (step part1) grid moves)]
-    print $ sum [100 * r + c | (C r c, '[') <- Map.toList (foldl (step part2) (expand grid) moves)]
-
-parse :: String -> (Map Coord Char, Maybe [Coord])
-parse inp =
-    ( Map.fromList $ coordLines (lines l)
-    , sequence $ fmap charToCoord =<< lines (trimEnd r)
-    )
-  where
-    [l, r] = splitOn "\n\n" inp
-
-expand :: Map Coord Char -> Map Coord Char
-expand grid =
-    Map.fromList $
-        concat
-            [ [(k * multiply, vl), (right (k * multiply), vr)]
-            | (k, v) <- Map.toList grid
-            , let (vl, vr) = newTile v
-            , let multiply = C 1 2
+    (input, moves') <- [format|2024 15 (%s%n)*%n(%s%n)*|]
+    let Just moves = mapM charToCoord (concat moves')
+    print $
+        sum
+            [ 100 * r + c
+            | (C r c, 'O') <-
+                Map.toList (foldl' (step part1) (buildGrid input) moves)
             ]
+    print $
+        sum
+            [ 100 * r + c
+            | (C r c, '[') <-
+                Map.toList (foldl' (step part2) (buildGrid (fmap expand input)) moves)
+            ]
+
+buildGrid :: [[Char]] -> Map Coord Char
+buildGrid = Map.fromList . coordLines
+
+expand :: String -> String
+expand = concatMap newTile
   where
-    newTile '#' = ('#', '#')
-    newTile 'O' = ('[', ']')
-    newTile '.' = ('.', '.')
-    newTile '@' = ('@', '.')
-    newTile _ = error "error tile"
+    newTile '#' = "##"
+    newTile 'O' = "[]"
+    newTile '.' = ".."
+    newTile '@' = "@."
+    newTile _ = error "bad input"
 
 at :: Map a Char -> a
 at grid = head [k | (k, v) <- Map.toList grid, v == '@']
 
 part2 :: Map Coord Char -> Coord -> Coord -> [(Coord, Char)]
 part2 grid p d@(C 0 _) = part1 grid p d
-part2 grid p d =
-    let moved = dfs [p] [p + d]
-     in ((\x -> (x + d, grid ! x)) <$> moved) -- place boxes at new pos
-            <> ((,'.') <$> moved) -- remove boxes from old pos
+part2 grid p d = [(p + d, grid ! p) | p <- moved] <> [(p, '.') | p <- moved]
   where
+    moved = dfs [p] [p + d]
     dfs :: [Coord] -> [Coord] -> [Coord]
     dfs acc [] = acc
-    dfs acc (p : ps)
-        | p `elem` acc = dfs acc ps
-        | me == '[' = dfs (p : acc) (right p : p + d : ps)
-        | me == ']' = dfs (p : acc) (left p : p + d : ps)
-        | me == '.' = dfs acc ps
-        | me == '@' = dfs acc ps
-        | otherwise = []
-      where
-        me = grid ! p
+    dfs acc (pos : ps)
+        | pos `elem` acc = dfs acc ps
+        | otherwise = case grid ! pos of
+            '#' -> []
+            '[' -> dfs (pos : acc) (right pos : pos + d : ps)
+            ']' -> dfs (pos : acc) (left pos : pos + d : ps)
+            _ -> dfs acc ps
 
 part1 :: Map Coord Char -> Coord -> Coord -> [(Coord, Char)]
-part1 grid p d = go [] p d
+part1 grid p d = go [] p
   where
-    go acc p d
-        | me == '.' = acc
-        | me == '#' = []
-        | otherwise = go ((p + d, me) : acc) (p + d) d
-      where
-        me = grid ! p
+    go acc p = case grid ! p of
+        '.' -> acc
+        '#' -> []
+        c -> go ((p + d, c) : acc) (p + d)
 
 updates :: Map Coord Char -> Coord -> [(Coord, Char)] -> Map Coord Char
 updates grid _ [] = grid
