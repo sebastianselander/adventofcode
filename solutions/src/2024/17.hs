ca37700cc42e5ab9b0c8a242f87f0694a7aad9cc
sebastianselander <sebastian.selander@gmail.com>
2024-12-17 14:35:41 +0100

    clean up day 17 part 1

--- solutions/src/2024/17.hs
+++ solutions/src/2024/17.hs
@@ -7,23 +7,18 @@
 
 module Main where
 
-import Advent.Format (format, format')
-import Advent.Prelude (fromDigits, toDigits)
-import Control.Monad (when)
-import Control.Monad.State (MonadState, State, evalState, execState, get, gets, modify)
+import Advent.Format (format)
+import Control.Monad.State (MonadState, State, execState, get, gets, modify)
 import Data.Array (Array, array, bounds, inRange, (!))
 import Data.Bits (xor)
-import Data.Tuple.Extra (fst3, snd3, thd3)
-import Debug.Trace (trace, traceShow)
+import Data.Function (fix)
+import Debug.Trace (traceShowId)
 
 main :: IO ()
 main = do
-    (a, b, c, instrs') <- [format|2024 17 Register A: %u%nRegister B: %u%nRegister C: %u%n%nProgram: %i&,%n|]
+    (a, _, _, instrs') <- [format|2024 17 Register A: %u%nRegister B: %u%nRegister C: %u%n%nProgram: %i&,%n|]
     let instrs = array (0 :: Int, length instrs' - 1) (zip [0 ..] instrs')
-    print $ run instrs (a, b, c) 0
-
-base8 :: [Int] -> Int
-base8 = fromDigits 8
+    print $ runInterpreter (initProgram instrs a)
 
 initProgram :: Array Int Int -> Int -> Program
 initProgram instructions a = Program instructions a 0 0 0 []
@@ -44,7 +39,12 @@ op_instruction = do
     pure (prg._instructions ! prg._pointer, prg._instructions ! (prg._pointer + 1))
 
 incPtr :: State Program ()
-incPtr = modify $ \s -> s{pointer = s.pointer + 2}
+incPtr = do
+    n <- gets _pointer
+    setPtr (n + 2)
+
+setPtr :: (MonadState Program m) => Int -> m ()
+setPtr n = modify $ \s -> s{_pointer = n}
 
 reg_a, reg_b, reg_c :: State Program Int
 reg_a = gets _reg_a
@@ -52,70 +52,71 @@ reg_b = gets _reg_b
 reg_c = gets _reg_c
 
 put_a, put_b, put_c :: Int -> State Program ()
-put_a x = modify $ \s -> s {_reg_a = x} 
-put_b x = modify $ \s -> s {_reg_b = x} 
-put_c x = modify $ \s -> s {_reg_c = x} 
+put_a x = modify $ \s -> s{_reg_a = x}
+put_b x = modify $ \s -> s{_reg_b = x}
+put_c x = modify $ \s -> s{_reg_c = x}
+
+out :: Int -> State Program ()
+out n = modify $ \s -> s{_out = n : s._out}
+
+runInterpreter :: Program -> [Int]
+runInterpreter program = reverse $ _out $ flip execState program $ do
+    fix $ \loop -> do
+        ptr <- gets _pointer
+        is <- gets _instructions
+        if not (inRange (bounds is) ptr) 
+            then pure ()
+            else do
+                interpret
+                loop
 
 interpret :: State Program ()
 interpret = do
-    (i, o) <- op_instruction
+    (i, o) <- traceShowId <$> op_instruction
     case i of
-        0 -> undefined
-    incPtr
+        0 -> do
+            a <- reg_a
+            o' <- combo o
+            put_a (a `div` (2 ^ o'))
+            incPtr
+        1 -> do
+            b <- reg_b
+            put_b (b `xor` o)
+            incPtr
+        2 -> do
+            o' <- combo o
+            put_b $ o' `mod` 8
+            incPtr
+        3 -> do
+            a <- reg_a
+            if a /= 0 then setPtr o else incPtr
+        4 -> do
+            b <- reg_b
+            c <- reg_c
+            put_b (xor b c)
+            incPtr
+        5 -> do
+            o' <- combo o
+            out $ o' `mod` 8
+            incPtr
+        6 -> do
+            a <- reg_a
+            o' <- combo o
+            put_b (a `div` (2 ^ o'))
+            incPtr
+        7 -> do
+            a <- reg_a
+            o' <- combo o
+            put_c (a `div` (2 ^ o'))
+            incPtr
+        _ -> error "bad input"
 
 combo :: Int -> State Program Int
-combo 0 = 0
-combo 1 = 1
-combo 2 = 2
-combo 3 = 3
-combo 4 = reg_a 
+combo 0 = pure 0
+combo 1 = pure 1
+combo 2 = pure 2
+combo 3 = pure 3
+combo 4 = reg_a
 combo 5 = reg_b
 combo 6 = reg_c
 combo _ = error "combo: bad input"
-
-run :: Array Int Int -> (Int, Int, Int) -> Int -> [Int]
-run instrs regs pointer = reverse $ flip execState [] $ go regs pointer
-  where
-    go :: (Int, Int, Int) -> Int -> State [Int] ()
-    go regs pointer = do
-        (pointer', regs') <- interpret instrs regs pointer
-        when (inRange (bounds instrs) (fromIntegral pointer')) $ go regs' pointer'
-
--- interpret :: Array Int Int -> (Int, Int, Int) -> Int -> State [Int] (Int, (Int, Int, Int))
--- interpret instrs (a, b, c) pointer =
---     let (inst, operand) = (instrs ! pointer, instrs ! (pointer + 1))
---         pointer' = pointer + 2
---      in case inst of
---             0 -> pure (pointer', (a `div` 2 ^ combo (a, b, c) operand, b, c))
---             1 -> pure (pointer', (a, xor b operand, c))
---             2 -> pure (pointer', (a, combo (a, b, c) operand `mod` 8, c))
---             3
---                 | a == 0 -> pure (pointer', (a, b, c))
---                 | operand /= pointer -> pure (operand, (a, b, c))
---                 | otherwise -> pure (pointer', (a, b, c))
---             4 -> pure (pointer', (a, xor b c, c))
---             5 -> do
---                 emit (combo (a, b, c) operand `mod` 8)
---                 pure (pointer', (a, b, c))
---             6 -> pure (pointer', (a, a `div` 2 ^ combo (a, b, c) operand, c))
---             7 -> pure (pointer', (a, b, a `div` 2 ^ combo (a, b, c) operand))
---             v -> error $ "bad input: " <> show v
-
-base2 :: Int -> [Int]
-base2 = toDigits 2
-
-base10 :: [Int] -> Int
-base10 = fromDigits 2
-
-emit :: (MonadState [a] m) => a -> m ()
-emit e = modify $ \s -> e : s
-
-combo :: (Int, Int, Int) -> Int -> Int
-combo _ 0 = 0
-combo _ 1 = 1
-combo _ 2 = 2
-combo _ 3 = 3
-combo regs 4 = fst3 regs
-combo regs 5 = snd3 regs
-combo regs 6 = thd3 regs
-combo _ v = error $ "combo: bad input: " <> show v
