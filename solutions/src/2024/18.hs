8fe230a724676f7b8c957513adfa6da81956da0c
sebastianselander <sebastian.selander@gmail.com>
2024-12-18 14:01:34 +0100

    drop 1024, use scanl

--- solutions/src/2024/18.hs
+++ solutions/src/2024/18.hs
@@ -3,9 +3,9 @@ module Main where
 import Advent.Coord (Coord (C), boundingBox, cardinal, contains)
 import Advent.Format (format)
 import Algorithm.Search (bfs)
-import Data.List (findIndex, inits)
+import Data.List (findIndex)
 import Data.Maybe (fromJust, isNothing)
-import Data.Set (fromList, notMember)
+import Data.Set (insert, notMember)
 import Data.Tuple.Extra (swap)
 
 main :: IO ()
@@ -13,9 +13,9 @@ main = do
     input <- [format|2024 18 (%u,%u%n)*|]
     let coords = fmap (uncurry C . swap) input
     let box@(start, end) = boundingBox coords
-    let path c blocks = bfs next (== end) c
+    let path c blocks = length <$> bfs next (== end) c
           where
             next curr = [ns | ns <- cardinal curr, box `contains` ns, ns `notMember` blocks]
-    let paths = fmap (path start . fromList) (drop 1 (inits coords))
-    print $ length $ fromJust (paths !! 1024)
-    putStrLn $ tail $ init $ show $ input !! fromJust (findIndex isNothing paths)
+    let paths' = tail $ scanl (\(_, acc) x -> (path start $ insert x acc, insert x acc)) (Nothing, mempty) coords
+    print $ fromJust (fst $ paths' !! 1024)
+    putStrLn $ tail $ init $ show $ input !! (1024 + fromJust (findIndex (isNothing . fst) (drop 1024 paths')))
