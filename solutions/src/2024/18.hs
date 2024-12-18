c0fe436a0b933905b17f9bad0ca38fed3ce5eb3f
sebastianselander <sebastian.selander@gmail.com>
2024-12-18 13:42:55 +0100

    update day 18

--- solutions/src/2024/18.hs
+++ solutions/src/2024/18.hs
@@ -1,22 +1,21 @@
 module Main where
 
-import Advent.Coord (Coord (..), boundingBox, cardinal, contains)
+import Advent.Coord (Coord (C), boundingBox, cardinal, contains)
 import Advent.Format (format)
 import Algorithm.Search (bfs)
 import Data.List (findIndex, inits)
 import Data.Maybe (fromJust, isNothing)
-import Data.Set (Set, fromList, notMember)
+import Data.Set (fromList, notMember)
 import Data.Tuple.Extra (swap)
 
 main :: IO ()
 main = do
     input <- [format|2024 18 (%u,%u%n)*|]
-    let coords = uncurry C . swap <$> input
-    let box = boundingBox coords
-    let path :: Coord -> Set Coord -> Maybe [Coord]
-        path c blocks = bfs next (== snd box) c
+    let coords = fmap (uncurry C . swap) input
+    let box@(start, end) = boundingBox coords
+    let path c blocks = bfs next (== end) c
           where
-            next c' = [ns | ns <- cardinal c', box `contains` ns, ns `notMember` blocks]
-    let paths = fmap (path (C 0 0) . fromList) (drop 1 (inits coords)) -- inits is slow
+            next curr = [ns | ns <- cardinal curr, box `contains` ns, ns `notMember` blocks]
+    let paths = fmap (path start . fromList) (drop 1 (inits coords))
     print $ length $ fromJust (paths !! 1024)
-    putStrLn $ (\(x, y) -> show x <> "," <> show y) $ input !! fromJust (findIndex isNothing paths)
+    putStrLn $ tail $ init $ show $ input !! fromJust (findIndex isNothing paths)
