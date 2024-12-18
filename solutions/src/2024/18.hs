80ee353a23a48cdb10deb024749f306edbcf2f0b
sebastianselander <sebastian.selander@gmail.com>
2024-12-18 06:41:18 +0100

    format, rename

--- solutions/src/2024/18.hs
+++ solutions/src/2024/18.hs
@@ -5,13 +5,13 @@ import Advent.Format (format)
 import Algorithm.Search (bfs)
 import Data.List (findIndex, inits, intercalate)
 import Data.Maybe (fromJust, isNothing)
-import Data.Set (Set, notMember, fromList)
+import Data.Set (Set, fromList, notMember)
 import Data.Tuple.Extra (swap)
 
 main :: IO ()
 main = do
     input <- fmap (uncurry C . swap) <$> [format|2024 18 (%u,%u%n)*|]
-    print $ length $ fromJust $ walk (C 0 0) (fromList $ take 1024 input)
+    print $ length $ fromJust $ path (C 0 0) (fromList $ take 1024 input)
     putStrLn $
         intercalate "," $
             (\(C r c) -> [show c, show r]) $
@@ -21,13 +21,13 @@ main = do
                             !! pred
                                 ( fromJust $
                                     findIndex isNothing $
-                                        fmap (walk (C 0 0) . fromList) (inits input)
+                                        fmap (path (C 0 0) . fromList) (inits input)
                                 )
 
 box :: (Coord, Coord)
 box = (C 0 0, C 70 70)
 
-walk :: Coord -> Set Coord -> Maybe [Coord]
-walk c blocks = bfs next (== snd box) c
+path :: Coord -> Set Coord -> Maybe [Coord]
+path c blocks = bfs next (== snd box) c
   where
     next c' = [ns | ns <- cardinal c', box `contains` ns, ns `notMember` blocks]
