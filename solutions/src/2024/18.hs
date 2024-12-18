5477a0ffff8be916c4add866535b2dfe3a5fcc4e
sebastianselander <sebastian.selander@gmail.com>
2024-12-18 07:04:55 +0100

    update day 18

--- solutions/src/2024/18.hs
+++ solutions/src/2024/18.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Advent.Coord (Coord (..), cardinal, contains)
+import Advent.Coord (Coord (..), boundingBox, cardinal, contains)
 import Advent.Format (format)
 import Algorithm.Search (bfs)
 import Data.List (findIndex, inits)
@@ -12,20 +12,11 @@ main :: IO ()
 main = do
     input <- [format|2024 18 (%u,%u%n)*|]
     let coords = uncurry C . swap <$> input
-    print $ length $ fromJust $ path (C 0 0) (fromList $ take 1024 coords)
-    putStrLn $
-        (\(x, y) -> show x <> "," <> show y) $
-            input
-                !! pred
-                    ( fromJust $
-                        findIndex isNothing $
-                            fmap (path (C 0 0) . fromList) (inits coords)
-                    )
-
-box :: (Coord, Coord)
-box = (C 0 0, C 70 70)
-
-path :: Coord -> Set Coord -> Maybe [Coord]
-path c blocks = bfs next (== snd box) c
-  where
-    next c' = [ns | ns <- cardinal c', box `contains` ns, ns `notMember` blocks]
+    let box = boundingBox coords
+    let path :: Coord -> Set Coord -> Maybe [Coord]
+        path c blocks = bfs next (== snd box) c
+          where
+            next c' = [ns | ns <- cardinal c', box `contains` ns, ns `notMember` blocks]
+    let paths = fmap (path (C 0 0) . fromList) (drop 1 (inits coords)) -- inits is slow
+    print $ length $ fromJust (paths !! 1024)
+    putStrLn $ (\(x, y) -> show x <> "," <> show y) $ input !! fromJust (findIndex isNothing paths)
