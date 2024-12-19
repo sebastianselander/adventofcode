bdc7f29d083159c4499ccb01c11992ae04ca2128
sebastianselander <sebastian.selander@gmail.com>
2024-12-19 07:13:23 +0100

    prettier day 19

--- solutions/src/2024/19.hs
+++ solutions/src/2024/19.hs
@@ -7,18 +7,14 @@ import Data.MemoTrie (memo2)
 
 main :: IO ()
 main = do
-    (colors', onsens) <- [format|2024 19 %s&( )%n%n(%s%n)*|]
-    let colors = fmap (filter (/= ',')) colors'
+    (colors, onsens) <- [format|2024 19 %s&(, )%n%n(%s%n)*|]
     let combinations = fmap (dp colors) onsens
-    print $ countBy (>0) combinations
+    print $ countBy (> 0) combinations
     print $ sum combinations
-
-patternMatch :: String -> [String] -> [String]
-patternMatch want available = [drop (length av) want | av <- available, av `isPrefixOf` want]
 
 dp :: [String] -> String -> Int
 dp = memo2 go
   where
-    go available x = case patternMatch x available of
-        [] -> 0
-        xs -> countBy null xs + sum (fmap (dp available) xs)
+    go available want = countBy null next + sum (fmap (dp available) next)
+      where
+        next = [drop (length av) want | av <- available, av `isPrefixOf` want]
