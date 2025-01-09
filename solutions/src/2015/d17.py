cd32d860972b11a22e0fc8e7d6216396db9f610e
sebastianselander <sebastian.selander@gmail.com>
2025-01-09 13:54:57 +0100

    2015 17 cleaner

--- solutions/src/2015/d17.py
+++ solutions/src/2015/d17.py
@@ -22,6 +22,6 @@ def dp(s, remain,cont):
     return dp(s[:-1], remain, cont) + dp(s[:-1],remain - s[-1], cont_)
 
 print(len(dp(s,150,())))
-xs = list((map(lambda x: (len(tuple(sorted(x)))),(dp(s,150,())))))
+xs = [len(tuple(sorted(x))) for x in dp(s,150,())]
 m = min(xs)
 print(len([x for x in xs if x == m]))
