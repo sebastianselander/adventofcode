a5a4282808ccc65c715085c5a67b649ad495dc6f
sebastianselander <sebastian.selander@gmail.com>
2025-01-10 19:24:18 +0100

    cleaner d17 2015

--- solutions/src/2015/d17.py
+++ solutions/src/2015/d17.py
@@ -1,6 +1,4 @@
 import functools
-import itertools
-
 
 with open("inputs/2015/17.txt") as f:
     s = f.read().strip()
@@ -13,7 +11,7 @@ s = tuple(s)
 @functools.cache
 def dp(s, remain,cont):
     if remain == 0:
-        return [list(cont)]
+        return [(cont)]
     if len(s) == 0:
         return []
     if s[-1] > remain:
@@ -21,7 +19,8 @@ def dp(s, remain,cont):
     cont_ = tuple(list(cont) + [s[-1]])
     return dp(s[:-1], remain, cont) + dp(s[:-1],remain - s[-1], cont_)
 
-print(len(dp(s,150,())))
-xs = [len(tuple(sorted(x))) for x in dp(s,150,())]
+res = dp(s,150,())
+print(len(res))
+xs = [len(tuple(sorted(x))) for x in res]
 m = min(xs)
 print(len([x for x in xs if x == m]))
