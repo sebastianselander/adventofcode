4ff4e5743ad2fd723835ccadc85cc576d03132cb
sebastianselander <sebastian.selander@gmail.com>
2025-01-10 19:24:12 +0100

    match case

--- solutions/src/2015/d07.py
+++ solutions/src/2015/d07.py
@@ -1,4 +1,4 @@
-import functools
+from functools import cache
 
 with open("../../../inputs/2015/07.txt") as f:
     s = f.read().strip()
@@ -6,40 +6,44 @@ with open("../../../inputs/2015/07.txt") as f:
 wires = {}
 
 for line in s.split("\n"):
-    lhs,rhs = line.split(" -> ")
+    lhs, rhs = line.split(" -> ")
     lhs = lhs.split()
-    if "AND" in lhs:
-        wires[rhs] = ["AND", lhs[0], lhs[2]]
-    elif "OR" in lhs:
-        wires[rhs] = ["OR", lhs[0], lhs[2]]
-    elif "LSHIFT" in lhs:
-        wires[rhs] = ["LSHIFT", lhs[0], lhs[2]]
-    elif "RSHIFT" in lhs:
-        wires[rhs] = ["RSHIFT", lhs[0], lhs[2]]
-    elif "NOT" in lhs:
-        wires[rhs] = ["NOT", lhs[1]]
-    else:
-        wires[rhs] = lhs
+    match lhs:
+        case [l, "AND", r]:
+            wires[rhs] = ["AND", l, r]
+        case [l, "OR", r]:
+            wires[rhs] = ["OR", l, r]
+        case [l, "LSHIFT", r]:
+            wires[rhs] = ["LSHIFT", l, r]
+        case [l, "RSHIFT", r]:
+            wires[rhs] = ["RSHIFT", l, r]
+        case ["NOT", v]:
+            wires[rhs] = ["NOT", v]
+        case _:
+            wires[rhs] = lhs
 
-
-@functools.cache
-def eval(reg,part2):
-    if part2 is not None and reg == "b": return part2 
-    if reg.isdigit(): return (int(reg))
+@cache
+def eval(reg, part2):
+    if part2 is not None and reg == "b":
+        return part2
+    if reg.isdigit():
+        return int(reg)
     v = wires[reg]
-    if v[0] == "AND":
-        return eval(v[1],part2) & eval(v[2],part2)
-    elif v[0] == "OR":
-        return eval(v[1],part2) | eval(v[2],part2)
-    elif v[0] == "LSHIFT":
-        return eval(v[1],part2) << eval(v[2],part2)
-    elif v[0] == "RSHIFT":
-        return eval(v[1],part2) >> eval(v[2],part2)
-    elif v[0] == "NOT":
-        return eval(v[1],part2) ^ 65535
-    else:
-        return eval(v[0],part2)
+    match v[0]:
+        case "AND":
+            return eval(v[1], part2) & eval(v[2], part2)
+        case "OR":
+            return eval(v[1], part2) | eval(v[2], part2)
+        case "LSHIFT":
+            return eval(v[1], part2) << eval(v[2], part2)
+        case "RSHIFT":
+            return eval(v[1], part2) >> eval(v[2], part2)
+        case "NOT":
+            return eval(v[1], part2) ^ 65535
+        case _:
+            return eval(v[0], part2)
 
-res = eval("a",None)
+
+res = eval("a", None)
 print(res)
-print(eval("a",res))
+print(eval("a", res))
