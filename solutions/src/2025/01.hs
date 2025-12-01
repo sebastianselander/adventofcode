f37eec316792ad9b75122cee6739ffb3d85b6b91
Sebastian Selander <sebastian.selander@gmail.com>
2025-12-01 06:52:59 +0100

    cleaner maybe?

--- solutions/src/2025/01.hs
+++ solutions/src/2025/01.hs
@@ -1,24 +1,25 @@
 module Main where
 
-import Advent.Format (format, format', fmt)
+import Advent.Format (format)
 import Advent.Prelude (count)
-import Debug.Trace (traceShow)
 
 main :: IO ()
 main = do
-    s <- [format|2025 01 ((R|L)!%u%n)*|]
-    print $ count 0 $ rot 50 s
+    s <- [format|2025 01 (%c%u%n)*|]
+    print $ count 0 $ rot s
     print $ count 0 $ rot2 50 s
 
-rot :: Int -> [(String, Int)] -> [Int]
-rot _ [] = []
-rot code (("L", n):xs) = ((code - n) `mod` 100) : rot ((code - n) `mod` 100) xs
-rot code (("R", n):xs) = ((code + n) `mod` 100) : rot ((code + n) `mod` 100) xs
-rot _ _ = undefined
+f :: (Num a) => Char -> a -> a -> a
+f 'L' = (-)
+f 'R' = (+)
 
-rot2 :: Int -> [(String, Int)] -> [Int]
+rot :: [(Char, Int)] -> [Int]
+rot = scanl (\acc (dir, n) -> f dir acc n `mod` 100) 50
+
+rot2 :: Int -> [(Char, Int)] -> [Int]
 rot2 _ [] = []
-rot2 code ((dir, n):xs) = case dir of
-    "L" | n > 0 -> ((code - 1) `mod` 100) : rot2 ((code - 1) `mod` 100) ((dir, n - 1):xs)
-    "R" | n > 0 -> ((code + 1) `mod` 100) : rot2 ((code + 1) `mod` 100) ((dir, n - 1):xs)
-    _ -> rot2 code xs
+rot2 code ((dir, n) : xs)
+    | n > 0 = new : rot2 new ((dir, n - 1) : xs)
+    | otherwise = rot2 code xs
+  where
+    new = f dir code 1 `mod` 100
