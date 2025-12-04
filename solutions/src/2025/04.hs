59c87e93cf83ec5fe07961e806c49c8082852894
Sebastian Selander <sebastian.selander@gmail.com>
2025-12-04 07:09:21 +0100

    clean up

--- solutions/src/2025/04.hs
+++ solutions/src/2025/04.hs
@@ -1,33 +1,24 @@
 module Main where
 
-import Advent.Coord
-import Advent.Format
-import Data.Array
+import Advent.Coord (Coord, contains, neighborsOn)
+import Advent.Format (getArrayInput)
+import Data.Array (Array, bounds, indices, (!), (//))
 import Data.Maybe (mapMaybe)
 
 main :: IO ()
 main = do
     s <- getArrayInput 2025 4
-    print $ length $ accessible s
-    print $ run s
+    print $ head $ sim s
+    print $ sum $ sim s
 
 access :: Coord -> Array Coord Char -> Maybe Coord
-access c arr =
-    if (arr ! c == '@')
-        && ( 4
-                > length
-                    ( filter
-                        (== '@')
-                        ([arr ! cs | cs <- neighborsOn (contains (bounds arr)) c])
-                    )
-           )
-        then Just c
-        else Nothing
+access c arr
+    | arr ! c == '@'
+    , length ([() | cs <- neighborsOn (contains (bounds arr)) c, arr ! cs == '@']) < 4 =
+        Just c
+    | otherwise = Nothing
 
-accessible :: Array Coord Char -> [Coord]
-accessible arr = mapMaybe (`access` arr) (indices arr)
-
-run :: Array Coord Char -> Int
-run arr = if null x then 0 else length x + run (arr // map (,'.') x)
+sim :: Array Coord Char -> [Int]
+sim arr = if null x then [] else length x : sim (arr // map (,'.') x)
   where
-    x = accessible arr
+    x = mapMaybe (`access` arr) (indices arr)
