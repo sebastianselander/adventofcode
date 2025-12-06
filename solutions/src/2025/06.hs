d4a61a5895cc0c3bd432ed74baa8a00d1479337a
Sebastian Selander <sebastian.selander@gmail.com>
2025-12-06 07:19:28 +0100

    more clean up

--- solutions/src/2025/06.hs
+++ solutions/src/2025/06.hs
@@ -8,15 +8,14 @@ import Data.List.Extra (splitOn)
 main :: IO ()
 main = do
     xs <- lines <$> getRawInput 2025 6
-    let (ints, ops) = (fmap read . words <$> init xs, words $ last xs)
-    print $ sum $ fmap (uncurry calc) (zip (transpose ints) ops)
-    print $ sum (solve <$> splitOn ["     "] (transpose xs))
+    let ints = fmap read . words <$> init xs
+    let ops = words $ last xs
+    print $ sum $ fmap (uncurry calculate) (zip (transpose ints) ops)
+    print $ sum (solve <$> splitOn [replicate (length xs) ' '] (transpose xs))
 
 solve :: [String] -> Int
-solve xs =
-    let ys = fmap (read @Int . filter isDigit) xs
-     in calc ys [last $ head xs]
+solve xs@(x:_) = calculate (fmap (read . filter isDigit) xs) [last $ x]
 
-calc :: [Int] -> String -> Int
-calc xs "*" = product xs
-calc xs "+" = sum xs
+calculate :: [Int] -> String -> Int
+calculate xs "*" = product xs
+calculate xs "+" = sum xs
