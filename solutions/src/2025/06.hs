28ddd4df5c09b2034b3c7a4ceaa5b33eb6fbeb33
Sebastian Selander <sebastian.selander@gmail.com>
2025-12-06 07:11:53 +0100

    clean day 6

--- solutions/src/2025/06.hs
+++ solutions/src/2025/06.hs
@@ -7,17 +7,15 @@ import Data.List.Extra (splitOn)
 
 main :: IO ()
 main = do
-    (ints, ops) <- [format'|2025 6 (( *%u *)*%n)*( *(\*|\+)! *)*%n|]
-    xs <- transpose . lines <$> getRawInput 2025 6
+    xs <- lines <$> getRawInput 2025 6
+    let (ints, ops) = (fmap read . words <$> init xs, words $ last xs)
     print $ sum $ fmap (uncurry calc) (zip (transpose ints) ops)
-    print $ sum (solve <$> splitOn ["     "] xs)
+    print $ sum (solve <$> splitOn ["     "] (transpose xs))
 
 solve :: [String] -> Int
 solve xs =
     let ys = fmap (read @Int . filter isDigit) xs
-     in case last (head xs) of
-            '+' -> sum ys
-            '*' -> product ys
+     in calc ys [last $ head xs]
 
 calc :: [Int] -> String -> Int
 calc xs "*" = product xs
