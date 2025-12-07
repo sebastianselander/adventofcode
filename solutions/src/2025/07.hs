9f55b93208d72061f7e94290413f85e7ce2f403a
Sebastian Selander <sebastian.selander@gmail.com>
2025-12-07 09:00:46 +0100

    clean up day 7 (or make it worse)

--- solutions/src/2025/07.hs
+++ solutions/src/2025/07.hs
@@ -1,10 +1,10 @@
 module Main where
 
-import Advent.Coord
-import Advent.Format
-import Advent.Prelude
-import Data.Array
-import Data.MemoTrie
+import Advent.Coord (Coord, below, coordArray, left, right)
+import Advent.Format (format)
+import Advent.Prelude ((!?))
+import Data.Array (Array, assocs)
+import Data.MemoTrie (memo)
 import Data.Set (Set)
 import Data.Set qualified as Set
 
@@ -12,28 +12,30 @@ main :: IO ()
 main = do
     xs <- coordArray @Array <$> [format|2025 7 (%y%n)*|]
     let start = head [x | (x, 'S') <- assocs xs]
-    print $ dfs xs [start] mempty
-    print $ dfs2 xs start
+    let (p1, p2) = unzip $ dfs xs [start] mempty
+    print $ sum p1
+    print $ head p2
 
-step :: Coord -> Array Coord Char -> (Int, [Coord])
+step :: Coord -> Array Coord Char -> [Coord]
 step c xs = case xs !? bel of
-    Nothing -> (0, [])
-    Just '^' -> (1, [right bel, left bel])
-    Just _ -> (0, [bel])
+    Nothing -> []
+    Just '^' -> [right bel, left bel]
+    Just _ -> [bel]
   where
     bel = below c
 
-dfs :: Array Coord Char -> [Coord] -> Set Coord -> Int
-dfs _ [] _ = 0
+dfs :: Array Coord Char -> [Coord] -> Set Coord -> [(Int, Int)]
+dfs _ [] _ = []
 dfs arr (x : xs) seen
     | x `Set.member` seen = dfs arr xs seen
     | otherwise = case step x arr of
-        (n, ys) -> n + dfs arr (ys <> xs) (Set.insert x seen)
+        ys ->
+            (max 0 (length ys - 1), sum (fmap (search arr) ys))
+                : dfs arr (ys <> xs) (Set.insert x seen)
 
-dfs2 :: Array Coord Char -> Coord -> Int
-dfs2 arr = memo go
+search :: Array Coord Char -> Coord -> Int
+search arr = memo go
   where
-    go x =
-        case step x arr of
-            (_, []) -> 1
-            (_, ys) -> sum $ fmap (dfs2 arr) ys
+    go x = case step x arr of
+        [] -> 1
+        ys -> sum $ fmap (search arr) ys
