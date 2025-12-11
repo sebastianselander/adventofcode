603fb61b5498283f206cf9f4769a7cac8ed62571
Sebastian Selander <sebastian.selander@gmail.com>
2025-12-11 07:47:06 +0100

    clean 11.hs

--- solutions/src/2025/11.hs
+++ solutions/src/2025/11.hs
@@ -1,12 +1,9 @@
 module Main where
 
 import Advent.Format (format)
-import Data.Map (Map)
 import Data.Map qualified as Map
 import Data.MemoTrie (memo2)
-import Data.Time.Clock
 
--- 0.028 - 0.034
 main :: IO ()
 main = do
     m <- Map.fromList <$> [format|2025 11 (%s: (%s& )%n)*|]
@@ -14,11 +11,7 @@ main = do
         go x n
             | x == "out" = if n >= 2 then 1 else 0
             | Just ns <- Map.lookup x m =
-                sum
-                    [ countPaths v n'
-                    | v <- ns
-                    , let n' = if x `elem` ["dac", "fft"] then n + 1 else n
-                    ]
+                sum [countPaths v n' | let n' = if x `elem` ["dac", "fft"] then n + 1 else n, v <- ns]
             | otherwise = 0
-    print $ countPaths "you" 2
-    print $ countPaths "svr" 0
+    print (countPaths "you" 2)
+    print (countPaths "svr" 0)
