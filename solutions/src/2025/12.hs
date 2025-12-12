0054cb36f3e526cdb5cadfea818f11af27c7df65
Sebastian Selander <sebastian.selander@gmail.com>
2025-12-12 07:29:40 +0100

    use foldr

--- solutions/src/2025/12.hs
+++ solutions/src/2025/12.hs
@@ -4,5 +4,5 @@ import Advent.Format (format)
 
 main :: IO ()
 main = do
-    (_, rows) <- [format|2025 12 ($(~(%u:)%n(%y+%n)*%n))*((%ux%u:)( %u)*%n)*|]
-    print $ sum $ fmap (\((a, b), xs) -> fromEnum $ a * b >= sum xs * 9) rows
+    rows <- [format|2025 12 ~($((%u:)%n(%y+%n)*%n))*((%ux%u:)( %u)*%n)*|]
+    print $ foldr (\((a, b), xs) -> (fromEnum (a * b >= sum xs * 9) +)) 0 rows
